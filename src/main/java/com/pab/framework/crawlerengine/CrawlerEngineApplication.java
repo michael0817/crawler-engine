d5eaff80b2f16eb15ea19f1816f0c2b16eb5cf7a
xuminxuan <michael0817@126.com>
2018-11-16 15:15:41 +0800

    add mapperscan

--- src/main/java/com/pab/framework/crawlerengine/CrawlerEngineApplication.java
+++ src/main/java/com/pab/framework/crawlerengine/CrawlerEngineApplication.java
@@ -1,15 +1,18 @@
 package com.pab.framework.crawlerengine;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAutoConfiguration
 @EnableTransactionManagement
 @SpringBootApplication
+@MapperScan("com.pab.framework.crawlerdb.dao")
 @EnableCaching
 public class CrawlerEngineApplication {
 
