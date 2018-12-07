1244a6bf73ee50b31a8a7d8a11d212a5966ca23d
javacar <fandongbao850113@sina.com>
2018-12-07 19:03:59 +0800

    任务具体逻辑移除

--- src/main/java/com/pab/framework/crawlerengine/task/NewsTaskHandler.java
+++ src/main/java/com/pab/framework/crawlerengine/task/NewsTaskHandler.java
@@ -1,11 +1,6 @@
 package com.pab.framework.crawlerengine.task;
 
-import com.pab.framework.crawlerdb.dao.*;
-import com.pab.framework.crawlerdb.domain.*;
-import com.pab.framework.crawlerengine.processor.DetailPageProcessor;
-import com.pab.framework.crawlerengine.processor.action.ActionProcessor;
-import com.pab.framework.crawlerengine.util.CrawlerUtil;
-import com.pab.framework.crawlerengine.util.FileUtils;
+import com.pab.framework.crawlerengine.processor.flow.FlowProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -31,84 +24,14 @@ public class NewsTaskHandler implements TaskHandler {
     ExecutorService threadFactory;
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
-    private CrawlerFlowInfoDao crawlerFlowInfoDao;
-    @Autowired
-    private CrawlerFlowDetailDao crawlerFlowDetailDao;
-    @Autowired
-    private CrawlerActionInfoDao crawlerActionInfoDao;
-    @Autowired
-    private CrawerActionDynamicInfoDao crawerActionDynamicInfoDao;
-    @Autowired
-    private CrawlerNewsMilestoneDao crawlerNewsMilestoneDao;
-    @Autowired
-    private ActionProcessor actionProcessor;
-
-    @Autowired
-    private DetailPageProcessor detailPageProcessor;
-
+    private FlowProcessor flowProcessor;
     @Scheduled(cron = "0  0 8 * * ?")
     @Override
     public void taskRun() {
         threadFactory.execute(new Runnable() {
             @Override
             public void run() {
-                List<CrawlerFlowInfo> crawlerFlowInfos = crawlerFlowInfoDao.findAll();
-                List<CrawlerFlowDetail> crawlerFlowDetails;
-                CrawlerActionInfo crawlerActionInfo;
-                List<CrawlerArticle> crawlerArticles;
-                String urlAddr;
-                Integer urlType;
-                List<String> urlAddrs=null;
-                String baseUrlAddr;
-                CrawerActionDynamicInfo crawerActionDynamicInfo;
-                CrawlerNewsMilestone crawlerNewsMilestone;
-
-                String actionDesc;
-
-                for (CrawlerFlowInfo crawlerFlowInfo : crawlerFlowInfos) {
-                    crawlerFlowDetails = crawlerFlowDetailDao.findAllByFlowId(crawlerFlowInfo.getFlowId());
-                    for (CrawlerFlowDetail crawlerFlowDetail : crawlerFlowDetails) {
-                        crawlerActionInfo = crawlerActionInfoDao.findOneByActionById(crawlerFlowDetail.getActionId());
-                        int actionType = crawlerActionInfo.getActionType();
-
-                        switch (actionType){
-                            case 2:
-                              urlAddrs=  actionProcessor.getUrlAddrs(crawlerActionInfo);
-                              break;
-                            case 3:
-                                baseUrlAddr=crawlerActionInfo.getBaseUrlAddr();
-                                urlType=crawlerActionInfo.getUrlType();
-                                urlAddr=crawlerActionInfo.getUrlAddr();
-                                crawlerActionInfo.getUrlType();
-                                int actionId = CrawlerUtil.extractIntOfStr(urlAddr, "[", "]");
-                                crawerActionDynamicInfoDao.deleteAll(actionId);
-                                int size=urlAddrs.size();
-                                for (int i = 0; i < size; i++) {
-                                    crawerActionDynamicInfo=new CrawerActionDynamicInfo();
-                                    crawerActionDynamicInfo.setActionId(actionId);
-                                    crawerActionDynamicInfo.setActionType(actionType);
-                                    crawerActionDynamicInfo.setUrlType(urlType);
-                                    crawerActionDynamicInfo.setUrlAddr(urlAddrs.get(i));
-                                    crawerActionDynamicInfoDao.insertAll(crawerActionDynamicInfo);
-                                    crawlerNewsMilestone = new CrawlerNewsMilestone();
-                                    crawlerNewsMilestone.setActionId(actionId);
-                                    crawlerNewsMilestone.setUrlAddr(urlAddrs.get(i));
-                                    int existsUrl = crawlerNewsMilestoneDao.isExistsUrl(crawlerNewsMilestone);
-                                    if (existsUrl == 0) {
-                                        crawlerNewsMilestoneDao.insertAll(crawlerNewsMilestone);
-                                    }
-                                }
-                                urlAddrs = crawlerNewsMilestoneDao.findUrlAddrsByNewDate(actionId);
-                                crawlerArticles = detailPageProcessor.process(baseUrlAddr, urlAddrs);
-                                actionDesc=crawlerActionInfo.getActionDesc();
-                                try {
-                                    FileUtils.write(actionDesc, crawlerArticles);
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                        }
-                    }
-                }
+                flowProcessor.start();
             }
         });
     }
