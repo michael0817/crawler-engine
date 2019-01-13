92938113d3f63aa2c97178286e65629058f21db0
xuminxuan <michael0817@126.com>
2019-01-13 11:08:09 +0800

    sync

--- src/test/java/Application.java
+++ src/test/java/Application.java
@@ -1,42 +1,35 @@
 import com.pab.framework.crawlerengine.CrawlerEngineApplication;
-import com.pab.framework.crawlerengine.processor.flow.FlowProcessor;
-import com.pab.framework.crawlerengine.service.PdfService;
-import com.pab.framework.crawlerengine.service.ProxyService;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.io.IOException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = CrawlerEngineApplication.class)
 public class Application {
-    @Autowired
-    private FlowProcessor flowProcessor;
-    @Autowired
-    private ProxyService proxyConfig;
-    @Autowired
-    private PdfService pdfService;
-
-    @Test
-    public void test() {
-        try {
-            flowProcessor.run();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testProxy(){
-        proxyConfig.getNewProxyIp();
-    }
-
-
-    @Test
-    public void testPdf(){
-        pdfService.generateNewsFile();
-    }
+//    @Autowired
+//    private FlowProcessor flowProcessor;
+//    @Autowired
+//    private ProxyService proxyConfig;
+//    @Autowired
+//    private PdfService pdfService;
+//
+//    @Test
+//    public void test() {
+//        try {
+//            flowProcessor.run();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    public void testProxy(){
+//        proxyConfig.getNewProxyIp();
+//    }
+//
+//
+//    @Test
+//    public void testPdf(){
+//        pdfService.generateNewsFile();
+//    }
 }
