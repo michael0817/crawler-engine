28406b44234a44c540b4dcffa44d2181c79b75b3
xiangneng <pingan@pingan>
2019-03-11 17:19:42 +0800

    sql脚本

--- crawler_db_2019-03-11.sql
+++ crawler_db_2019-03-11.sql
@@ -1,6 +1,6 @@
-DROP TABLE crawler_action_info;
-DROP TABLE crawler_flow_detail;
-DROP TABLE crawler_flow_info;
+delete from crawler_action_info;
+delete from crawler_flow_detail;
+delete from crawler_flow_info;
 INSERT INTO `crawler_action_info` VALUES (1, '政策法规-目录', 2, 1, '/chinese/newListDoc/111003/{1~2}.html', 'http://www.cbrc.gov.cn', '//table[@id=\'testUI\']//a[@target=\'_blank\']/@href||//table[@id=\'testUI\']//a[@target=\'_blank\']/text()||//table[@id=\'testUI\']//a[@target=\'_blank\']');
 INSERT INTO `crawler_action_info` VALUES (2, '政策法规-资讯', 4, 1, '[action_id_1]', 'http://www.cbrc.gov.cn', '/html/head/title/text()||||/html/body//div[@id=\'docTitle\']/div[1]/text()||/html/body//div[@class=\'Section0\']/p/tidyText()');
 INSERT INTO `crawler_action_info` VALUES (3, '监管动态-目录', 2, 1, '/chinese/newListDoc/111005/{1~2}.html', 'http://www.cbrc.gov.cn', '//table[@id=\'testUI\']//a[@target=\'_blank\']/@href||//table[@id=\'testUI\']//a[@target=\'_blank\']/text()||//table[@id=\'testUI\']//a[@target=\'_blank\']');
@@ -28,10 +28,10 @@ INSERT INTO `crawler_action_info` VALUES (24, '雪球金融-目录', 2, 2, '/v4/
 INSERT INTO `crawler_action_info` VALUES (25, '雪球金融-资讯', 4, 1, '[action_id_23]', 'https://xueqiu.com', 'a[@class=\'time\']/text(),h1[@class=\'article__bd__title\']/text(),div[@id=\'app\']');
 INSERT INTO `crawler_action_info` VALUES (26, '虎嗅微语-目录', 2, 1, '/member/1453857/article/{1~2}.html', 'https://www.huxiu.com', '/article/\\d+\\.html\\?f=member_article');
 INSERT INTO `crawler_action_info` VALUES (27, '虎嗅微语-目录', 4, 1, '[action_id_25]', 'https://www.huxiu.com', 'span[@class=\'article-time\']/text(),h1[@class=\'t-h1\']/text(),div[@class=\'article-section-wrap\']');
-INSERT INTO `crawler_action_info` VALUES (28, '招行掌上商城-一级目录', 2, 1, '/_CL5_/Category/GetAllCategories', 'https://ssl.mall.cmbchina.com', '//div[@id=\'divC1Panel\']//li/a/@data-id||//div[@id=\'divC1Panel\']//li/a/text()||//div[@id=\'divC1Panel\']//li/a');
-INSERT INTO `crawler_action_info` VALUES (29, '招行掌上商城-二级目录', 2, 1, '/_CL5_/Category/GetCategories?id={5~14}', 'https://ssl.mall.cmbchina.com', '//dl[@class=\'catalog_list\']//dd/a/@c3id||//dl[@class=\'catalog_list\']//dd/a/span/text()||//dl[@class=\'catalog_list\']//dd/a');
-INSERT INTO `crawler_action_info` VALUES (30, '招行掌上商城-商品目录', 2, 1, '/_CL5_/Product/ProductListAjaxLoad?subCategory=[action_id_29]&navigationKey=&sort=70&pageIndex={1~2}', 'https://ssl.mall.cmbchina.com', '//div[@class=\'item-detail\']//p[@class=\'clearfix\']/span[@class=\'fr grand-total\']/@productid||//div[@class=\'item-detail\']/h4/text()||//div[@class=\'item-detail\']/a');
-INSERT INTO `crawler_action_info` VALUES (31, '招行掌上商城-手机目录', 2, 1, '/_CL5_/Product/ProductListAjaxLoad?subCategory=370&navigationKey=&sort=70&pageIndex={1~9}', 'https://ssl.mall.cmbchina.com', '//div[@class=\'item-detail\']//p[@class=\'clearfix\']/span[@class=\'fr grand-total\']/@productid||//div[@class=\'item-detail\']/h4/text()||//div[@class=\'item-detail\']//a/@href');
+INSERT INTO `crawler_action_info` VALUES (28, '招行掌上商城-一级目录', 6, 1, '/_CL5_/Category/GetAllCategories', 'https://ssl.mall.cmbchina.com', '//div[@id=\'divC1Panel\']//li/a/@data-id||//div[@id=\'divC1Panel\']//li/a/text()||//div[@id=\'divC1Panel\']//li/a');
+INSERT INTO `crawler_action_info` VALUES (29, '招行掌上商城-二级目录', 6, 1, '/_CL5_/Category/GetCategories?id={5~14}', 'https://ssl.mall.cmbchina.com', '//dl[@class=\'catalog_list\']//dd/a/@c3id||//dl[@class=\'catalog_list\']//dd/a/span/text()||//dl[@class=\'catalog_list\']//dd/a');
+INSERT INTO `crawler_action_info` VALUES (30, '招行掌上商城-商品目录', 6, 1, '/_CL5_/Product/ProductListAjaxLoad?subCategory=[action_id_29]&navigationKey=&sort=70&pageIndex={1~2}', 'https://ssl.mall.cmbchina.com', '//div[@class=\'item-detail\']//p[@class=\'clearfix\']/span[@class=\'fr grand-total\']/@productid||//div[@class=\'item-detail\']/h4/text()||//div[@class=\'item-detail\']/a');
+INSERT INTO `crawler_action_info` VALUES (31, '招行掌上商城-手机目录', 6, 1, '/_CL5_/Product/ProductListAjaxLoad?subCategory=370&navigationKey=&sort=70&pageIndex={1~99}', 'https://ssl.mall.cmbchina.com', '//div[@class=\'item-detail\']//p[@class=\'clearfix\']/span[@class=\'fr grand-total\']/@productid||//div[@class=\'item-detail\']/h4/text()||//div[@class=\'item-detail\']//a/@href');
 INSERT INTO `crawler_action_info` VALUES (32, '焦点新闻-目录', 2, 1, '/news', 'http://www.51kaxun.com', '//div[@id=\'news_top\']//ul//a/@href||//div[@id=\'news_top\']//ul//a/text()||//div[@id=\'news_top\']//ul//a');
 INSERT INTO `crawler_action_info` VALUES (33, '焦点新闻-资讯', 4, 1, '[action_id_32]', 'http://www.51kaxun.com', '/html/body//h1[@class=\'nr01_tit\']/text()||/html/body//div[@class=\'yhym\']/p[1]/text(2)||||/html/body//div[@class=\'right01_nr\']/tidyText()');
 INSERT INTO `crawler_action_info` VALUES (34, '业界新闻-目录', 2, 1, '/news/search.php?id=1&p={1~2}', 'http://www.51kaxun.com', '//ul[@id=\'news_list\']//a/@href||//ul[@id=\'news_list\']//a/text()||//ul[@id=\'news_list\']//a');
